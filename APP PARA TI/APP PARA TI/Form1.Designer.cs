030b36c1a2eba9d21a591b9410c44c20c6d67766
victAll <vitormanuel.allende@gmail.com>
2016-05-20 17:02:22 +0200

    Mira y pincha a ver si funciona

---
+++ APP PARA TI/APP PARA TI/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace APP_PARA_TI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bPinchar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bPinchar
+            // 
+            this.bPinchar.BackColor = System.Drawing.Color.Red;
+            this.bPinchar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.bPinchar.Location = new System.Drawing.Point(53, 184);
+            this.bPinchar.Name = "bPinchar";
+            this.bPinchar.Size = new System.Drawing.Size(150, 47);
+            this.bPinchar.TabIndex = 0;
+            this.bPinchar.Text = "PINCHAR";
+            this.bPinchar.UseVisualStyleBackColor = false;
+            this.bPinchar.Click += new System.EventHandler(this.bPinchar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.bPinchar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bPinchar;
+    }
+}
+
---
+++ APP PARA TI/APP PARA TI/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APP_PARA_TI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bPinchar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("HOLA CABRONCETE JEJEJEJE");
+        }
+    }
+}
